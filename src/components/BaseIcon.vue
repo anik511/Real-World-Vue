5bcdb66efa640f4b21184357b3ab12c0605c8a40
anik511 <anikdatta151051@gmail.com>
2021-03-24 21:54:06 +0600

    first commit

---
+++ src/components/BaseIcon.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <b-icon icon="people-fill" variant="secondary"></b-icon>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ src/views/EventList.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>Events Listing</h1>
+    <event-card
+      v-for="event in events"
+      :key="event.id"
+      :event="event"
+    ></event-card>
+    <!-- <BaseIcon /> -->
+  </div>
+</template>
+<script>
+import EventCard from "@/components/EventCard";
+import EventService from "@/services/EventService";
+export default {
+  components: {
+    EventCard,
+  },
+  data() {
+    return {
+      events: [],
+    };
+  },
+  created() {
+    EventService.getEvents()
+      .then((response) => {
+        console.log(response.data);
+        this.events = response.data;
+      })
+      .catch((error) => {
+        console.log("Axios Error in EventList: ", error.response);
+      });
+  },
+};
+</script>
+
+<style></style>
